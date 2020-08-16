6fdb9a054b42859e366e6d862e8cb15206e0bc4c
arif93sulaiman <arifhip1993@yahoo.com>
2020-08-16 22:01:14 +0800

    stage 3

--- controller/User.js
+++ controller/User.js
@@ -7,7 +7,7 @@ const userController = {
         return res.status(200).json({ success: true, message : `job well done ${id}`})
     },
     createUser: (req, res, next ) => {
-        const { firstName, lastName, email } = req.body
+        const { firstName, lastName, email, userType } = req.body
         let error = {}
         let errorMessage = ""
         let isBodyValid = true
@@ -24,6 +24,18 @@ const userController = {
             isBodyValid = false
             error['email'] = "this should be an email & not empty"
         }
+        if (!userType || typeof userType !== "string" || userType.trim().length === 0) {
+            isBodyValid = false
+            error['userType'] = "user type should be a string & not empty"
+        }else{
+            const checks = ['user', 'admin', 'support']
+            //check if its found in the array return true or false
+            const found = checks.some(check => check === userType)
+            if (!found) {
+                isBodyValid = false
+                error['userType'] = 'this should be a string & can have these values' + checks.join(',')
+            }
+        }
         if (!isBodyValid) {
             return res.status(400).json({
             success: isBodyValid,
@@ -37,6 +49,7 @@ const userController = {
             firstName,
             lastName,
             email,
+            userType
         }
             
         
